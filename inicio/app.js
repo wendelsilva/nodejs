24d57cb41e4d52442347d271a1469776cf14cec3
wendelsilva <wendel245.ws@gmail.com>
2021-08-17 21:01:23 -0300

    hello world in nodejs

---
+++ inicio/app.js
@@ -0,0 +1,10 @@
+var mostrarSite = true;
+const site = "https://github.com/wendelsilva/nodejs";
+
+console.log("hello world");
+console.log("Meu nome é Wendel");
+console.log("E eu estou aprendendo Node.js com o Guia do programador");
+
+if (mostrarSite) {
+    console.log(site);
+}

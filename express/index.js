f54869571d40b806bf88e2052e0acd71e73e7598
wendelsilva <wendel245.ws@gmail.com>
2021-08-24 21:01:31 -0300

    working with params e query params

--- express/index.js
+++ express/index.js
@@ -6,13 +6,34 @@ app.get("/", (req, res) => {
     res.send("<h1>Bem vindo ao meu repositorio de estudos em nodejs</h1>"); // enviando uma resposta
 }); // criando a rota recebendo o req(requisicao) e o res(resposta)
 
-app.get("/blog", (req, res) => {
-    res.send("<h3>bem vindo ao meu blog: https://github.com/wendelsilva</h3>")
-})
+app.get("/blog/:artigo?", (req, res) => {
+
+    var artigo = req.params.artigo; // recebendo o parametro da url
+
+    if(artigo) {
+        res.send("<h1>Artigo: " + artigo + "</h1>");
+    } else {
+        res.send("<h3>bem vindo ao meu blog: https://github.com/wendelsilva</h3>")
+    }
+}) // passando parametro não obrigatorio nome na rota
 
 app.get("/canal/youtube", (req, res) => {
-    res.send("<h1>bem vindo ao meu canal do youtube</h1>")
+    var canal = req.query["canal"]; // recebendo o parametro da url
+
+    if(canal) {
+        res.send("<h1>canal: " + canal + "</h1>");
+    } else {
+        res.send("<h3>Nenhum canal fornecido</h3>")
+    }
 })
+
+app.get("/ola/:nome/:empresa", (req, res) => {
+    // req são os dados enviados pelo usario
+    // res são os dados que serao enviados para o usuario
+    var nome = req.params.nome; // recebendo o parametro nome
+    var empresa = req.params.empresa; // recebendo o parametro empresa
+    res.send("<h1>Oi " + nome + " do " + empresa + " </h1>");
+}) // passando parametro obrigatorio nome na rota
 
 app.listen(4000, erro => {
     if(erro) {

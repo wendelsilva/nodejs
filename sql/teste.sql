ab07980191213f5f18f084e5c24e0ef1dadb0067
wendelsilva <wendel245.ws@gmail.com>
2021-09-21 21:46:43 -0300

    study(mysql): comeback to mysql

---
+++ sql/teste.sql
@@ -0,0 +1,29 @@
+CREATE TABLE usuarios(
+    nome VARCHAR(50),
+    email VARCHAR(100),
+    idade INT
+);
+
+INSERT INTO usuarios(nome, email, idade) VALUES (
+    'Wendel',
+    'wendel@teste.com',
+    19
+);
+
+INSERT INTO usuarios(nome, email, idade) VALUES (
+    'Ella',
+    'ella@teste.com',
+    23
+);
+
+INSERT INTO usuarios(nome, email, idade) VALUES (
+    'Mel',
+    'mel@teste.com',
+    1
+);
+
+INSERT INTO usuarios(nome, email, idade) VALUES (
+    'Luna',
+    'luna@teste.com',
+    5
+);

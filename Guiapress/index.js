2131c8ceb39543a3181dc58d4726c25dd5afdfad
wendelsilva <wendel245.ws@gmail.com>
2022-06-02 15:27:55 -0300

    feat: Create and List users

--- Guiapress/index.js
+++ Guiapress/index.js
@@ -5,9 +5,11 @@ const connection = require('./database/database');
 
 const categoriesController = require('./categories/CategoriesController');
 const articlesController = require('./articles/ArticlesController');
+const usersController = require('./users/UsersController');
 
 const Article = require('./articles/Article');
 const Category = require('./categories/Category');
+const User = require('./users/User');
 
 // View engine setup
 app.set('view engine', 'ejs');
@@ -32,6 +34,7 @@ connection
 
 app.use("/", categoriesController);
 app.use("/", articlesController);
+app.use("/", usersController);
 
 app.get('/', (req, res) => {
   Article.findAll({

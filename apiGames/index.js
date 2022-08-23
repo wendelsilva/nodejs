b8d19e1272ce408ec08198ea3b07c1105d1ad586
wendelsilva <wendel245.ws@gmail.com>
2022-08-23 18:32:05 -0300

    refactor apiGames and use axios to consume api

--- apiGames/index.js
+++ apiGames/index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
@@ -57,7 +59,7 @@ app.post("/game", (req, res) => {
         id: 3,
         title,
         year,
-        price,
+        price
     });
 
     res.sendStatus(201);

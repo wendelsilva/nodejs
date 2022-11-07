a7827753f2891aacfac17ba92e732b3cb793133c
wendelsilva <wendel245.ws@gmail.com>
2022-11-07 17:58:03 -0300

    users api part 2 finalized

--- user-api/controllers/UserController.js
+++ user-api/controllers/UserController.js
@@ -1,7 +1,21 @@
 var User = require("../models/User")
 class UserController {
 
-    async index(req, res) {};
+    async index(req, res) {
+        var users = await User.findAll();
+        res.json(users);
+    };
+
+    async findUser(req, res) {
+        var id = req.params.id;
+        var user = await User.findById(id)
+
+        if(user == undefined) {
+            res.status(404).send({})
+        } else {
+            res.json(user)
+        }
+    }
 
     async create(req, res) {
         var {name, email, password, role} = req.body;
@@ -20,6 +34,32 @@ class UserController {
         await User.create(email, password, name)
         res.status(200).send();
     };
+
+    async edit(req, res) {
+        var {id, name, role, email} = req.body;
+        var result = await User.update(id, email, name, role);
+        if(result != undefined) {
+            if(result.status) {
+                res.send("updated successfully")
+            } else {
+                res.status(406).send(result.error)
+            }
+        } else {
+            res.status(406).send(result.error)
+        }
+    }
+
+    async remove(req, res) {
+        var id = req.params.id;
+
+        var result = await User.delete(id);
+
+        if(result) {
+            res.send("user deleted")
+        } else {
+            res.send(result.error)
+        }
+    }
 }
 
 module.exports = new UserController();
--- user-api/models/User.js
+++ user-api/models/User.js
@@ -2,6 +2,30 @@ var knex = require("../database/connection");
 var bcrypt = require("bcrypt");
 class User {
 
+    async findAll() {
+        try {
+            var result = await knex.select(["id","name","email","role"]).table("users");
+            
+            if(result.length > 0) {
+                return result[0];
+            } else {
+                return undefined;
+            }
+
+        } catch (error) {
+            return undefined;
+        }
+    }
+
+    async findById(id) {
+        try {
+            var result = await knex.select(["id","name","email","role"]).where({id:id}).table("users");
+            return result;
+        } catch (error) {
+            return [];
+        }
+    }
+
     async create(email, password, name) {
         try {
             var hash = await bcrypt.hash(password, 10);
@@ -25,6 +49,57 @@ class User {
         } catch (error) {
             console.log(error);
             return false;
+        }
+    }
+
+    async update(id, email, name, role) {
+        var user = await this.findById(id);
+
+        if(user != undefined) {
+            var editUser = {};
+
+            if(email != undefined) {
+                if(email != user.email) {
+                    var result = await this.findEmail(email)
+                    if(!result) {
+                        editUser.email = email;
+                    } else  {
+                        return {status: false, error: "email has already been assigned"};
+                    }
+                }
+            }
+
+            if(name != undefined) {
+                editUser.name = name;
+            }
+
+            if(role != undefined) {
+                editUser.role = role;
+            }
+
+            try {
+                await knex.update(editUser).where({id: id}).table("users");
+                return {status: true};
+            } catch (error) {
+                return {status: false, error: error};
+            }
+
+        } else {
+            return {status: false, error: "user not exist"};
+        }
+    }
+
+    async delete(id) {
+        var user = await this.findById(id);
+        if(user != undefined) {
+            try {
+                await knex.delete().where({id:id}).table("users")
+            } catch (error) {
+                return {status: false, error: "user not exist"};
+            }
+
+        } else {
+            return {status: false, error: "user not exist"};
         }
     }
 }
